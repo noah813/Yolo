c2235f06c562bbcbf56baf46668d90373be4ad88
Noah <noahlin813@gmail.com>
2024-10-16 18:11:53 +0800

    edited start_train.py

--- src/start_train.py
+++ src/start_train.py
@@ -9,6 +9,6 @@ model = YOLO("yolo11n.yaml").load("yolo11n.pt")
 print("Enter data path:")
 data = input()
 print("Enter train times:")
-times = input()
+times = int(input())
 
 results = model.train(data=data, epochs=times, imgsz=640, cache=True, device=0, )
