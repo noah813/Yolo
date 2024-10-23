f79c4118d187a2b836a105bd492252010094372d
Noah <noahlin813@gmail.com>
2024-10-23 18:44:45 +0800

    added datasets

--- src/Utils/utils.py
+++ src/Utils/utils.py
@@ -1,7 +1,7 @@
 def get_dataset_path(dataset_name):
     # 構建資料集路徑
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # 獲取 Yolo 文件夾的路徑
-    dataset_path = os.path.join(base_dir, 'dataset', dataset_name, 'data.yaml')
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../Datasets'))
+    dataset_path = os.path.join(base_path, dataset_name, 'data.yaml')
     
     # 檢查 data.yaml 是否存在
     if not os.path.exists(dataset_path):
